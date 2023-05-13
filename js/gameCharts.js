3bdb447bafbc9ab1d9ab96d8757ec84d14780e63
Jussi Ukkonen <jussiukkonen@outlook.com>
2023-05-13 17:30:22 +0300

    Update style

--- js/gameCharts.js
+++ js/gameCharts.js
@@ -37,19 +37,19 @@ dbRef.on("value", (snapshot) => {
 
         // Remove dublicates, sort by highest, cut to 5 elements
         dataTop5 = [...new Set(dataScores)].sort((a, b) => b - a);
-        dataTop5 = dataTop5.slice(0,5);
+        dataTop5 = dataTop5.slice(0, 5);
 
         // Count same elements
         dataScores.forEach(function (x) { dataValues[x] = (dataValues[x] || 0) + 1; });
         // Set empty elements to zero
         dataValues = Array.from(dataValues, item => item || 0);
-        
+
         // Count data amount
         dataCounts = Array.from(Array(dataValues.length).keys())
 
         // Averages
         dataMovingAverage = movingAverage(dataValues);
-        
+
         createCharts();
     }
     else console.log("No data");
@@ -82,17 +82,17 @@ function movingAverage(arr) {
 // Calculate average from array
 function average(arr) {
     var total = 0;
-    for(let value of arr) total += value;
-    return Math.round((total / arr.length)*10)/10;
+    for (let value of arr) total += value;
+    return Math.round((total / arr.length) * 10) / 10;
 }
 
 // CHARTS
 // -------------------------------------------------------------------------------------------------------
-const canvasChartTop5 = document.getElementById("chart1");
+const canvasChartTop5 = document.getElementById("gameChartsChart1");
 const ctxChartTop5 = canvasChartTop5.getContext("2d");
 let chartTop5 = null;
 
-const canvasChartAvarage = document.getElementById("chart2");
+const canvasChartAvarage = document.getElementById("gameChartsChart2");
 const ctxChartAvarage = canvasChartAvarage.getContext("2d");
 let chartAvarage = null;
 
@@ -104,7 +104,7 @@ function createChartTop5() {
     chartTop5 = new Chart(ctxChartTop5, {
         type: "bar",
         data: {
-            labels: [1,2,3,4,5],
+            labels: [1, 2, 3, 4, 5],
             datasets: [{
                 label: "Top 5",
                 data: dataTop5,
@@ -128,7 +128,7 @@ function createChartTop5() {
                 legend: {
                     labels: {
                         boxWidth: 0,
-                        padding: 15, 
+                        padding: 15,
                         color: "white",
                         font: {
                             size: 18,
@@ -147,7 +147,7 @@ function createChartTop5() {
             scales: {
                 x: {
                     grid: {
-                        display:false,
+                        display: false,
                     },
                     ticks: {
                         color: "rgb(180,180,180)"
@@ -157,7 +157,7 @@ function createChartTop5() {
                 y: {
                     grid: {
                         color: "rgb(80,80,80)",
-                        drawBorder:false
+                        drawBorder: false
                     },
                     ticks: {
                         color: "rgb(180,180,180)",
@@ -213,7 +213,7 @@ function createChartAverage() {
                 legend: {
                     labels: {
                         boxWidth: 20,
-                        padding: 15, 
+                        padding: 15,
                         color: "white",
                         font: {
                             size: 18,
@@ -232,7 +232,7 @@ function createChartAverage() {
             scales: {
                 x: {
                     grid: {
-                        display:false,
+                        display: false,
                     },
                     ticks: {
                         color: "rgb(180,180,180)"
@@ -242,7 +242,7 @@ function createChartAverage() {
                 y: {
                     grid: {
                         color: "rgb(80,80,80)",
-                        drawBorder:false
+                        drawBorder: false
                     },
                     ticks: {
                         color: "rgb(180,180,180)",
@@ -266,14 +266,14 @@ function createCharts() {
     else {
         createChartTop5();
         createChartAverage();
-    } 
+    }
 }
 // If resize, reset charts
 window.addEventListener("resize", createCharts);
 
 // GAME
 // -------------------------------------------------------------------------------------------------------
-const canvasGame = document.getElementById("game");
+const canvasGame = document.getElementById("gameChartsGame");
 const ctx = canvasGame.getContext("2d");
 let H = canvasGame.height = canvasGame.clientHeight;
 let W = canvasGame.width = canvasGame.clientWidth;
@@ -290,29 +290,41 @@ let writeScoreTrigger = false;
 let keySpace = false;
 let keyR = false;
 
+let keyP = false;
+let pause = false;
+
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
 function keyDownHandler(e) {
     event.preventDefault();
     // Space
-    if(e.keyCode == 32) {
-        keySpace = true;    
+    if (e.keyCode == 32) {
+        keySpace = true;
     }
     // R
-    if(e.keyCode == 82) {
-        keyR = true;    
+    if (e.keyCode == 82) {
+        keyR = true;
     }
-} 
+    // P
+    if (e.keyCode == 80 && gameState == 1) {
+        pause = !pause;
+    }
+
+}
 function keyUpHandler(e) {
     event.preventDefault();
     // Space
-    if(e.keyCode == 32) {
+    if (e.keyCode == 32) {
         keySpace = false;
     }
     // R
-    if(e.keyCode == 82) {
+    if (e.keyCode == 82) {
         keyR = false;
+    }
+    // P
+    if (e.keyCode == 80) {
+        keyP = false;
     }
 }
 
@@ -320,19 +332,19 @@ function startMenu() {
     draw();
     ctx.font = "20px Arial";
     ctx.fillStyle = gameColor;
-    ctx.textAlign = "center"; 
-    ctx.fillText("Press SPACE to start", W/2, H/2);
+    ctx.textAlign = "center";
+    ctx.fillText("Press SPACE to start", W / 2, H / 2);
 }
 
 function gameOverMenu() {
     draw();
     ctx.font = "30px Arial";
     ctx.fillStyle = gameColor;
-    ctx.textAlign = "center"; 
-    ctx.fillText("Game Over", W/2, H/2 - 40);
+    ctx.textAlign = "center";
+    ctx.fillText("Game Over", W / 2, H / 2 - 40);
     ctx.font = "20px Arial";
-    ctx.fillText("Score: " + score, W/2, H/2 );
-    ctx.fillText("Press R to restart", W/2, H/2 + 40);
+    ctx.fillText("Score: " + score, W / 2, H / 2);
+    ctx.fillText("Press R to restart", W / 2, H / 2 + 40);
 }
 
 var score = 0;
@@ -344,7 +356,7 @@ function updateScore() {
 // Reset canvas & values
 function restart() {
     player.y = H - player.radius - 1;
-    obstacleDown.x = W + obstacleDown.width*2;
+    obstacleDown.x = W + obstacleDown.width * 2;
     obstacleDown.dx = W / 165;
     particles = [];
     score = 0;
@@ -367,7 +379,7 @@ function resize() {
 // Draw static objects 
 function draw() {
     // Top line 
-    ctx.lineWidth = 4; 
+    ctx.lineWidth = 4;
     ctx.strokeStyle = gameColor;
     ctx.moveTo(0, 0);
     ctx.lineTo(W, 0);
@@ -378,7 +390,7 @@ function draw() {
     ctx.moveTo(0, H);
     ctx.lineTo(W, H);
     ctx.stroke();
-} 
+}
 
 class Player {
     constructor() {
@@ -388,7 +400,7 @@ class Player {
         this.dy = 0;
         this.gravity = 0.2;
         this.gravitySpeed = 0;
-        this.isGrounded = false;                                                                
+        this.isGrounded = false;
     }
     draw() {
         ctx.shadowColor = playerColor;
@@ -407,8 +419,8 @@ class Player {
         if (keySpace) this.gravity = -0.6;
         else this.gravity = 0.2;
     }
-    borders() {                                       
-        var ground = H-this.radius;
+    borders() {
+        var ground = H - this.radius;
         var roof = this.radius;
         if (this.y > ground) {
             this.y = ground;
@@ -420,7 +432,7 @@ class Player {
         }
     }
     // Check for collisions. If position borders are same with collision object -> hit
-    collision (col) {
+    collision(col) {
         var offset = -1
         var myleft = this.x + offset;
         var myright = this.x + this.radius + offset;
@@ -430,15 +442,15 @@ class Player {
         var colright = col.x + col.width;
         var coltop = col.y;
         var colbottom = col.y + col.height;
-        
+
         var hit = true;
         if (mybottom < coltop || mytop > colbottom || myright < colleft || myleft > colright) hit = false;
         return hit;
-    }   
+    }
 }
 
 class Obstacle {
-    constructor (bool, height, y) {
+    constructor(bool, height, y) {
         this.isBottom = bool;
         this.width = 15;
         this.gap = 140;
@@ -452,16 +464,16 @@ class Obstacle {
             ctx.shadowBlur = 0;
             ctx.fillStyle = gameColor;
             // Bottom
-            if(bool) ctx.fillRect(this.x ,this.y, this.width, this.height);
+            if (bool) ctx.fillRect(this.x, this.y, this.width, this.height);
             // Top
-            else ctx.fillRect(this.x ,0, this.width, this.height);
+            else ctx.fillRect(this.x, 0, this.width, this.height);
         };
     }
 }
 
 var particles = [];
 class Particle {
-    constructor () {
+    constructor() {
         this.life = true;
         this.radius = player.radius;
         this.vRadius = 0;
@@ -471,17 +483,17 @@ class Particle {
         this.vx = getRandom(-3, -2.5);
         this.color = playerColor;
     }
-    move() { 
+    move() {
         this.x += this.vx;
         this.vRadius = 1;
         if (this.radius > 1) this.radius -= this.vRadius;
-        if (this.radius <= 1) this.life = false;       
+        if (this.radius <= 1) this.life = false;
     }
     draw() {
         ctx.shadowColor = playerColor;
         ctx.shadowBlur = 8;
-        ctx.beginPath();    
-        ctx.arc( this.x, this.y, this.radius, 0, 2 * Math.PI );
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
         ctx.closePath();
         ctx.fillStyle = this.color;
         ctx.fill();
@@ -489,24 +501,24 @@ class Particle {
 }
 
 let player = new Player();
-let startHeight = getRandom(40,140);
+let startHeight = getRandom(40, 140);
 let obstacleDown = new Obstacle(true, startHeight, H - startHeight);
 let obstacleTop = new Obstacle(false, H - startHeight - obstacleDown.gap, 0);
 
 // Move top and down obstacles
-function updateObstacles(down, top) { 
+function updateObstacles(down, top) {
     down.x -= down.dx;
     top.x = down.x;
     // If is over left border
-    if (down.x < -down.width*2) {
+    if (down.x < -down.width * 2) {
         down.dx += 0.2;
-        var randomHeight = getRandom(40,140);
+        var randomHeight = getRandom(40, 140);
         down.height = randomHeight;
-        down.y = H - randomHeight; 
+        down.y = H - randomHeight;
         top.height = H - randomHeight - top.gap;
-        score ++;
-        down.x = W + down.width*2;
-    }    
+        score++;
+        down.x = W + down.width * 2;
+    }
     // Draw obstacles
     top.draw();
     down.draw();
@@ -517,51 +529,55 @@ function updateParticles() {
     for (let i = particles.length - 1; i >= 0; i--) {
         particles[i].move();
         // If particle is dead, remove it        
-        if( !particles[i].life) particles.splice( i, 1 );
+        if (!particles[i].life) particles.splice(i, 1);
     }
     if (particles.length < 20) particles.push(new Particle());
     // Draw 
-    for (let i = particles.length - 1; i >= 0; i--) {   
+    for (let i = particles.length - 1; i >= 0; i--) {
         particles[i].draw();
-    }     
+    }
 }
 
 // Render canvas loop
 function render() {
-    ctx.clearRect(0, 0, W, H);
-    H = canvasGame.height = canvasGame.clientHeight;
-    W = canvasGame.width = canvasGame.clientWidth;
-    // Start menu
-    if (gameState == 0) {
-        startMenu();
-        if (keySpace) gameState = 1;
-    }
-    // Game
-    else if (gameState == 1) {
-        if (player.collision(obstacleDown) || player.collision(obstacleTop)) {
-            gameState = 2;
-        }     
-        draw();     
-        player.move();
-        player.draw();
-        updateParticles();
-        updateObstacles(obstacleDown, obstacleTop);
-        updateScore();
-    } 
-    // Game over
-    else if (gameState == 2) { 
-        gameOverMenu();
-        // Send score to database only once
-        if (!writeScoreTrigger) {
-            dbRef.push(score);
-            writeScoreTrigger = true;
+    if (!pause) {
+        ctx.clearRect(0, 0, W, H);
+        H = canvasGame.height = canvasGame.clientHeight;
+        W = canvasGame.width = canvasGame.clientWidth;
+        // Start menu
+        if (gameState == 0) {
+            startMenu();
+            if (keySpace) gameState = 1;
         }
-        if (!keySpace && keyR) restart();
+        // Game
+        else if (gameState == 1) {
+            if (player.collision(obstacleDown) || player.collision(obstacleTop)) {
+                gameState = 2;
+            }
+            draw();
+            player.move();
+            player.draw();
+            updateParticles();
+            updateObstacles(obstacleDown, obstacleTop);
+            updateScore();
+        }
+        // Game over
+        else if (gameState == 2) {
+            gameOverMenu();
+            pause = false;
+            // Send score to database only once
+            if (!writeScoreTrigger) {
+                dbRef.push(score);
+                writeScoreTrigger = true;
+            }
+            if (!keySpace && keyR) restart();
+        }
+
     }
-    window.requestAnimationFrame(render);      
-}    
+    window.requestAnimationFrame(render);
+}
 window.requestAnimationFrame(render);
-  
+
 // Return random float between min & max
 function getRandom(min, max) {
     return Math.random() * (max - min) + min;
